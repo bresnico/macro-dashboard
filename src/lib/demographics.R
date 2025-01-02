d34096ae3892c828564492643c0d53a94ba7c38d
Nicolas Bressoud <nicolas.bressoud@hepvs.ch>
2025-01-02 18:34:08 +0100

    préparé les fonctions démographiques et màj gitignore pour exclure les tests

---
+++ src/lib/demographics.R
@@ -0,0 +1,40 @@
+# Fonction pour traiter les données démographiques
+process_demographics <- function(data, config) {
+  # Construction des variables depuis la config 
+  demographic_fields <- list(
+    age = list(
+      id = paste0(config$demographics$age$group, config$demographics$age$question),
+      mapping = config$demographics$age$labels
+    ),
+    gender = list(
+      id = paste0(config$demographics$gender$group, config$demographics$gender$question),
+      mapping = config$demographics$gender$labels 
+    ),
+    status = list(
+      id = paste0(config$demographics$status$group, config$demographics$status$question),
+      mapping = config$demographics$status$labels
+    ),
+    experience = list(
+      id = paste0(config$demographics$experience$group, config$demographics$experience$question),
+      mapping = config$demographics$experience$labels
+    )
+  )
+  
+  # Préparation des données avec les mêmes identifiants que scales.R
+  demographics_data <- data %>%
+    mutate(
+      person_id = clean_teacher_id(g01q13),
+      person_id_secure = g01q13,
+      group_id = groupecode,
+      timestamp = as.Date(ymd_hms(datestamp)),
+      month = format(timestamp, "%B %Y"),
+      # Conversion des codes en labels pour chaque variable démographique
+      age = recode(!!sym(demographic_fields$age$id), !!!demographic_fields$age$mapping),
+      gender = recode(!!sym(demographic_fields$gender$id), !!!demographic_fields$gender$mapping),
+      status = recode(!!sym(demographic_fields$status$id), !!!demographic_fields$status$mapping),
+      experience = recode(!!sym(demographic_fields$experience$id), !!!demographic_fields$experience$mapping)
+    ) %>%
+    select(timestamp, month, person_id_secure, group_id, age, gender, status, experience)
+  
+  return(demographics_data)
+}
